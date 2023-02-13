41bf95929d813ec36b53d3b5e86773254b252f57
Yankee Knight <116320996+YankeeKnight@users.noreply.github.com>
2023-02-13 09:50:26 -0500

    finished models || started api routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,21 +4,60 @@ const { Category, Product } = require('../../models');
 // The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+  // finds all categories
+  Category.findAll({
+    include: {
+      model: Product,
+      attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+    }
+  }).then(cateData => {
+    if (!cateData) {
+      res.status(404).json({ message: 'Category not found.' });
+      return;
+    }
+    res.json(cateData);
+  }).catch(err => {
+    console.log(err);
+    res.status(500).json(err)
+  });
 });
 
 router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+  // finds one category by its `id` value
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: {
+      model: Product,
+      attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+    }
+  }).then(cateData => {
+    if (!cateData) {
+      res.status(404).json({ message: 'Category not found.' });
+      return;
+    }
+    res.json(cateData);
+  }).catch(err => {
+    console.log(err);
+    res.status(500).json(err)
+  });
 });
 
 router.post('/', (req, res) => {
-  // create a new category
+  // creates a new category
+  Category.create({
+    category_name: req.body.category_name
+  }).then(cateData => res.json(cateData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+  // updates a category by its `id` value
+
 });
 
 router.delete('/:id', (req, res) => {
