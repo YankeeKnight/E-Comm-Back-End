bff70209c5819f707aee9a627b18f5991e2b1236
Yankee Knight <116320996+YankeeKnight@users.noreply.github.com>
2023-02-15 20:18:47 -0500

    corrected issue in product-routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -84,35 +84,18 @@ router.post('/', (req, res) => {
 router.put('/:id', (req, res) => {
   Product.update(req.body, {
     where: {
-      id: req.params.id,
-    },
-  })
-    .then((product) => {
-      return ProductTag.findAll({ where: { product_id: req.params.id } });
-    })
-    .then((productTags) => {
-      const productTagIds = productTags.map(({ tag_id }) => tag_id);
-      const newProductTags = req.body.tagIds
-        .filter((tag_id) => !productTagIds.includes(tag_id))
-        .map((tag_id) => {
-          return {
-            product_id: req.params.id,
-            tag_id,
-          };
-        });
-      const productTagsToRemove = productTags
-        .filter(({ tag_id }) => !req.body.tagIds.includes(tag_id))
-        .map(({ id }) => id);
-
-      return Promise.all([
-        ProductTag.destroy({ where: { id: productTagsToRemove } }),
-        ProductTag.bulkCreate(newProductTags),
-      ]);
-    })
-    .then((updatedProductTags) => res.json(updatedProductTags))
-    .catch((err) => {
-      res.status(400).json(err);
-    });
+      id: req.params.id
+    }
+  }).then(prodData => {
+    if (!prodData) {
+      res.status(404).json({ message: 'Category by ID not found.' });
+      return;
+    }
+    res.json(prodData);
+  }).catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 // deletes one product by its `id` value
